cacba36934c7f44b9bfb2dc687014d1825ade742
Boris Kamstra <boriskamstra34@gmail.com>
2025-06-23 23:52:17 +0200

    Ongebruikt blok verwijderd

--- src/App.js
+++ src/App.js
@@ -6,63 +6,8 @@ import Intro from "./components/Intro";
 import Navigation from "./components/Navigation";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
-// import Socials from "./components/Socials";
 
 function App() {
-    // const scrolledInView = [
-    //     {
-    //         intro: false,
-    //         education: false,
-    //         skills: false,
-    //         about: false,
-    //         projects: false,
-    //         contact: false,
-    //     },
-    // ];
-
-    // function handleScroll() {
-    //     const intro = document
-    //         .getElementById("intro")
-    //         .getBoundingClientRect().top;
-    //     const education = document
-    //         .getElementById("education")
-    //         .getBoundingClientRect().top;
-    //     const skills = document
-    //         .getElementById("skills")
-    //         .getBoundingClientRect().top;
-    //     const about = document
-    //         .getElementById("about")
-    //         .getBoundingClientRect().top;
-    //     const projects = document
-    //         .getElementById("projects")
-    //         .getBoundingClientRect().top;
-    //     const contact = document
-    //         .getElementById("contact")
-    //         .getBoundingClientRect().top;
-
-    //     const halfScreenPoint = window.innerHeight / 2;
-
-    //     if (contact < halfScreenPoint) {
-    //         addFadeIn('contact');
-    //     } else if (projects < halfScreenPoint) {
-    //         addFadeIn('projects');
-    //     } else if (about < halfScreenPoint) {
-    //         addFadeIn('about');
-    //     } else if (skills < halfScreenPoint) {
-    //         addFadeIn('skills');
-    //     } else if (education < halfScreenPoint) {
-    //         addFadeIn('education');
-    //     } else if (intro < halfScreenPoint) {
-    //         addFadeIn('intro');
-    //     }
-    // }
-
-    // function addFadeIn(section) {
-    //     document.getElementById(section).classList.add('fadeIn')
-    // }
-
-    // window.addEventListener("scroll", handleScroll);
-
     return (
         <div className="wrapper">
             <Hero></Hero>
@@ -73,7 +18,6 @@ function App() {
                 <Skills></Skills>
                 <AboutWebsite></AboutWebsite>
                 <Projects></Projects>
-                {/* <Socials></Socials> */}
                 <Contact></Contact>
             </div>
         </div>
